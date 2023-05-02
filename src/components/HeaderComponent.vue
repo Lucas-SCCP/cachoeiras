4a566c2486b39cbc965c23b1e88422b8575078ef
Lucas Rodrigues <lucas.alves@rentcars.com>
2023-05-01 22:04:15 -0300

    Primeiro commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="bg-cover bg-center h-screen relative" style="background-image: url('../assets/images/cachoeira.png')">
+        <nav class="absolute top-0 left-0 right-0 p-4 flex justify-between items-center h-16 max-w-7xl mx-auto">
+            <div class="logo">
+                <img src="../assets/images/logo.png" alt="Logo" class="w-24 h-auto">
+            </div>
+            <div class="social-icons">
+                <ul class="flex items-center">
+                    <li>
+                        <a href="#" class="text-gray-100 hover:text-white text-lg px-2 py-1"><i class="fab fa-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-100 hover:text-white text-lg px-2 py-1"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-100 hover:text-white text-lg px-2 py-1"><i class="fab fa-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
